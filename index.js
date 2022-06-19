6daf2a1e45a33dafd91f1a8f9542573bdcc4131e
laureenakinyi <laureen.akinyi@student.moringaschool.com>
2022-06-20 00:17:59 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverseWord = word.split("").reverse().join("")
+  return word === reverseWord;
 }
 
 /* 
   Add your pseudocode here
+  First, I created a new variable, reverseWord
+  I split, reverse then join it toa string
+  If the reversed word, forward and backword is the same return true
+  Else false
 */
 
 /*
   Add written explanation of your solution here
+  I created a new variable, reverseWord
+  The code checks if the reversedWord is equal to word, returns true
+  else returns false if they are not equal
 */
 
 // You can run `node index.js` to view these console logs
